ba384f763ecea0049760a7e6f0edb1708202857d
khan <krmaryum@hotmail.com>
2025-12-21 22:17:27 -0600

    add day-1 script

---
+++ day-01/python_script_creation.py
@@ -0,0 +1,43 @@
+#You will create a Python script that:
+
+#Takes threshold values (CPU, disk, memory) from user input
+#Also fetches system metrics using a Python library (example: psutil)
+#Compares metrics against thresholds
+#Prints the result to the terminal
+
+import psutil
+
+# Get Threshold Valus from a User
+
+cpu_threshold = float(input("Enter CPU usage threshold (%): "))
+memory_threshold = float(input("Enter memory usage threshold (%): "))
+disk_threshold = float(input("Enter disk usage threshold (%): "))
+
+# Fetch current system_metrics
+
+cpu_usage = psutil.cpu_percent(interval=1)
+print(cpu_threshold)
+memory_usage = psutil.virtual_memory().percent
+print(memory_usage)
+disk_usage = psutil.disk_usage('/').percent
+print(disk_usage)
+
+# Compare usage against threshold and print results.
+
+# Compare CPU
+if cpu_usage > cpu_threshold:
+    print("[ALERT] CPU usage is HIGH")
+else:
+    print("[OK] CPU usage is normal")
+
+# Compare Memory
+if memory_usage > memory_threshold:
+    print("[ALERT] Memory usage is HIGH")
+else:
+    print("[OK] Memory usage is normal")
+
+# Compare Disk
+if disk_usage > disk_threshold:
+    print("[ALERT] Disk usage is HIGH")
+else:
+    print("[OK] Disk usage is normal")
